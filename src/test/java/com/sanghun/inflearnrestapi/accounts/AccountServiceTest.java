5827abd36dc7f617b682be1d8e8bfd5b0de07b12
sanghun.gwon <sanghun.gwon@nhn.com>
2022-04-29 16:48:44 +0900

    no message

--- src/test/java/com/sanghun/inflearnrestapi/accounts/AccountServiceTest.java
+++ src/test/java/com/sanghun/inflearnrestapi/accounts/AccountServiceTest.java
@@ -1,17 +1,21 @@
 package com.sanghun.inflearnrestapi.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Set;
 
@@ -19,6 +23,8 @@ import java.util.Set;
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -43,5 +49,18 @@ public class AccountServiceTest {
 
         //Then
         assertThat(userDetails.getPassword(), is(equalTo(password)));
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void findByUsernameFail() {
+        //expected
+        String userName = "test@naver.com";
+
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(userName));
+
+        //when
+        accountService.loadUserByUsername(userName);
+
     }
 }
